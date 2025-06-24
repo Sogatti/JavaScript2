a9b5970653c3f56186b6987236c5b1d8398596ad
曽我凌太 <sogaryota@sogaryoutanoMacBook-Air.local>
2025-06-25 06:45:47 +0900

    PRへの修正リクエストを修正しました

--- main2.js
+++ main2.js
@@ -19,15 +19,45 @@ function get_calc(btn) {
     }
 
     if (add_calc === ".") {
-
       const tokens = display.value.split(/[\+\-\*\/]/);
       const lastNumber = tokens[tokens.length - 1];
-
       if (lastNumber.includes(".")) {
         return;
       }
+      if (lastNumber === "") {
+        display.value += "0.";
+        return;
+      }
     }
+
+    if (add_calc === "00") {
+      const tokens = display.value.split(/[\+\-\*\/]/);
+      const lastNumber = tokens[tokens.length - 1];
+
+      if (lastNumber === "" || lastNumber === "0") {
+        return;
+      }
+    }
+
+    if (add_calc === "0") {
+      const tokens = display.value.split(/[\+\-\*\/]/);
+      const lastNumber = tokens[tokens.length - 1];
+
+      if (lastNumber === "0") {
+        return;
+      }
+    }
+
+    if ("123456789".includes(add_calc)) {
+      const tokens = display.value.split(/[\+\-\*\/]/);
+      const lastNumber = tokens[tokens.length - 1];
+
+      if (lastNumber === "0") {
+        display.value = display.value.slice(0, -1) + add_calc;
+        return;
+      }
+    }
+
     document.dentaku.display.value += add_calc;
   }
-
 }
